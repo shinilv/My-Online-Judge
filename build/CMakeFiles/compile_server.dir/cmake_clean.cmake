337468b7bc92a4a52d0b2914839c53691fb1374a
shinilv <19510296826@163.com>
2025-11-24 19:53:09 +0800

    _cmake_

---
+++ build/CMakeFiles/compile_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/root/Online_Judge/excu/compile_server"
+  "/root/Online_Judge/excu/compile_server.pdb"
+  "CMakeFiles/compile_server.dir/compile_server/compile_server.cc.o"
+  "CMakeFiles/compile_server.dir/compile_server/compile_server.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compile_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/oj_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/root/Online_Judge/excu/oj_server"
+  "/root/Online_Judge/excu/oj_server.pdb"
+  "CMakeFiles/oj_server.dir/oj_server/oj_server.cc.o"
+  "CMakeFiles/oj_server.dir/oj_server/oj_server.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oj_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
